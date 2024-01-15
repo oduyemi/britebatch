af88f2d2d946e9138f414ebfdf0b3ce88645b931
Yemi Oduyemi <yhermii@gmail.com>
2024-01-15 16:39:26 +0100

    Project setuo

---
+++ src/main/java/dev/yemi/britebatch/BritebatchApplication.java
@@ -0,0 +1,13 @@
+package dev.yemi.britebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritebatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yemi/britebatch/BritebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yemi.britebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BritebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
